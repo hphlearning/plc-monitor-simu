0ade6ea23a5bb26ae30c545f3b95c36fd0be57c2
hphlearning <1500775765@qq.com>
2025-12-23 16:42:50 +0800

    添加有问题的地方的解答

--- PLC-Address-Monitor/Form1.vb
+++ PLC-Address-Monitor/Form1.vb
@@ -32,6 +32,8 @@
         Dim currentVolt As Double = rand.Next(210, 230) ' 模拟电压 210-230V
 
         ' 2. 更新界面显示
+        ' 字符串内插 ($) 与 格式化 (F2)
+        ' {currentPress:F2} 表示将双精度浮点数强制保留 2 位小数，并自动四舍五入
         lblTemp.Text = $"温度: {currentTemp:F1} ℃"
         lblPress.Text = $"压力: {currentPress:F2} Mpa"
         lblVolt.Text = $"电压: {currentVolt:F1} V"
@@ -43,6 +45,12 @@
             lblTemp.ForeColor = Color.White
         Else
             ' 正常范围内，恢复默认颜色
+            ' ---【正常/复位状态】---
+            ' 问题点解答：为什么使用 SystemColors.Control？
+            'SystemColors.Control 是 Windows 窗体默认的背景颜色
+            ' 1. 它不是固定颜色，而是 Windows 系统定义的“控件默认底色”。
+            ' 2. 使用它能让 UI 风格在不同 Windows 主题下保持统一。
+            ' 3. 当报警消除，必须将背景“复位”到初始颜色，否则 Label 会一直显示红色。
             lblTemp.BackColor = SystemColors.Control
             lblTemp.ForeColor = Color.Black
         End If
