08c6333e7762ed3f3294cbed6346d242bd913d5f
hphlearning <1500775765@qq.com>
2025-12-23 16:23:18 +0800

    PLC-Address-Monitor

---
+++ PLC-Address-Monitor/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        lblTemp = New Label()
+        lblPress = New Label()
+        lblVolt = New Label()
+        tmrMonitor = New Timer(components)
+        btnStart = New Button()
+        SuspendLayout()
+        ' 
+        ' lblTemp
+        ' 
+        lblTemp.AutoSize = True
+        lblTemp.Location = New Point(261, 127)
+        lblTemp.Name = "lblTemp"
+        lblTemp.Size = New Size(104, 24)
+        lblTemp.TabIndex = 0
+        lblTemp.Text = "温度: 0.0 ℃"
+        ' 
+        ' lblPress
+        ' 
+        lblPress.AutoSize = True
+        lblPress.Location = New Point(261, 169)
+        lblPress.Name = "lblPress"
+        lblPress.Size = New Size(126, 24)
+        lblPress.TabIndex = 1
+        lblPress.Text = "压力: 0.0 Mpa"
+        ' 
+        ' lblVolt
+        ' 
+        lblVolt.AutoSize = True
+        lblVolt.Location = New Point(261, 216)
+        lblVolt.Name = "lblVolt"
+        lblVolt.Size = New Size(98, 24)
+        lblVolt.TabIndex = 2
+        lblVolt.Text = "电压: 0.0 V"
+        ' 
+        ' tmrMonitor
+        ' 
+        tmrMonitor.Interval = 500
+        ' 
+        ' btnStart
+        ' 
+        btnStart.Location = New Point(544, 157)
+        btnStart.Name = "btnStart"
+        btnStart.Size = New Size(112, 34)
+        btnStart.TabIndex = 3
+        btnStart.Text = "开始监视"
+        btnStart.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(11F, 24F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnStart)
+        Controls.Add(lblVolt)
+        Controls.Add(lblPress)
+        Controls.Add(lblTemp)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblTemp As Label
+    Friend WithEvents lblPress As Label
+    Friend WithEvents lblVolt As Label
+    Friend WithEvents tmrMonitor As Timer
+    Friend WithEvents btnStart As Button
+
+End Class
---
+++ PLC-Address-Monitor/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    ' 实例化随机数生成器，用于模拟 PLC 数据
+    Private rand As New Random()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' 初始化 Timer 设置
+        tmrMonitor.Interval = 500 ' 设置为500毫秒刷新一次
+        tmrMonitor.Enabled = False ' 初始状态不运行
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        tmrMonitor.Start()
+        btnStart.Enabled = False ' 防止重复点击
+        btnStart.Text = "监视运行中..."
+    End Sub
+
+    Private Sub lblTemp_Click(sender As Object, e As EventArgs) Handles lblTemp.Click
+
+    End Sub
+
+    Private Sub lblPress_Click(sender As Object, e As EventArgs) Handles lblPress.Click
+
+    End Sub
+
+    Private Sub lblVolt_Click(sender As Object, e As EventArgs) Handles lblVolt.Click
+
+    End Sub
+
+    Private Sub tmrMonitor_Tick(sender As Object, e As EventArgs) Handles tmrMonitor.Tick
+        ' 1. 模拟获取 PLC 数据 (生成随机数)
+        Dim currentTemp As Double = rand.Next(20, 100) ' 模拟温度 20-100度
+        Dim currentPress As Double = rand.NextDouble() * 10 ' 模拟压力 0-10Mpa
+        Dim currentVolt As Double = rand.Next(210, 230) ' 模拟电压 210-230V
+
+        ' 2. 更新界面显示
+        lblTemp.Text = $"温度: {currentTemp:F1} ℃"
+        lblPress.Text = $"压力: {currentPress:F2} Mpa"
+        lblVolt.Text = $"电压: {currentVolt:F1} V"
+
+        ' 3. 报警逻辑判断 (核心考点)
+        If currentTemp > 80 Then
+            ' 超过 80 度，触发红色背景报警
+            lblTemp.BackColor = Color.Red
+            lblTemp.ForeColor = Color.White
+        Else
+            ' 正常范围内，恢复默认颜色
+            lblTemp.BackColor = SystemColors.Control
+            lblTemp.ForeColor = Color.Black
+        End If
+    End Sub
+End Class
